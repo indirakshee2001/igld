fde877aad5a1978702379063492cf0574681fecf
indirakshee2001 <indrakshiguild@gmail.com>
2014-12-05 15:11:22 +0530

    Create memlogin.php
    First Version of the member login html page

---
+++ memlogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<link type="text/css" rel="stylesheet" href="guildmemberstyle.css" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <title>Add Email</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <p class="logintext">Login to <strong>The Indrakshi Guild</strong></p>
+  
+  <form method="post" action="memlogin.php">
+	<input type="text" name="usname" /> User Name <br/>
+	<input type="password" name="pword" />  Password <br/>	 
+   <input type="submit" value="Login" name="submitbutt" />
+  </form>
+ 
+<p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</p>
+</p>
+</body>
+
