2c34595018513e2cf5fc96b58744d2cf221b5987
indirakshee2001 <indrakshiguild@gmail.com>
2014-12-05 15:20:08 +0530

    Create memlogin1.php
    The First Version of Qualifying the Members and Sending to the next stage of the process

---
+++ memlogin1.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+  // Clear the error message
+$error_msg = "";
+  
+
+include 'includelocaldb.php';
+
+
+$user_username = $_POST['usname'];
+$user_password = $_POST['pword'];
+
+$reshapw = sha1($user_password);
+
+//echo $user_username;
+//echo $user_password;
+//echo $reshapw;
+
+	
+
+$query = "Select `username` from `members` where `members`.`username` = '{$user_username}' and `upword` = '{$reshapw}'"; 
+$data = mysql_query($query);
+$Pan_Date_Result =  mysql_result($data,0,0);
+
+
+
+		
+
+	
+if (mysql_num_rows($data) == 1) {
+	 $_SESSION['thisusername'] = $user_username;
+	 echo "You Have Successfully Logged in as " .$user_username;    	
+    header('Location: dateselectlandingmem.php');    
+    }
+     
+else {
+    // The username/password are incorrect so send the authentication headers
+        exit('<h2>Mismatch</h2>Sorry, you must enter a valid username and password to log in and access this page. If you ' .
+      'aren\'t a registered member, please <a href="joinmailing.html">sign up</a>.');
+  }
+  
+ 
+  
+?>
+
+
